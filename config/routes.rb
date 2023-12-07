24071056ec090d11f8e612404d0f394bf95df84a
MenendezON <menendezon@gmail.com>
2023-12-07 23:53:24 +0000

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "main#index"
+
+  resources :motorbikes, only: %i[show]
+
   namespace :api do
     namespace :v1 do
       resources :motorbikes
