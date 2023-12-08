c0e12e9087b91f627fad3d90fbc4889dede3359d
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-08 14:12:34 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
+# config/routes.rb
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+ 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+ 
   # Defines the root path route ("/")
   root "main#index"
-
+ 
   resources :motorbikes, only: %i[show]
-
+ 
   namespace :api do
     namespace :v1 do
       resources :motorbikes
     end
   end
-  
-end
+ 
+  # Add new route for "My Reservations"
+  get '/my_reservations', to: 'reservations#my_reservations'
+ end
+ 
