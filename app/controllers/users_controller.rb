f78b404feccd4323019ec320d5a1e81f7c2778ac
Mike Dark <agyemankojo47@gmail.com>
2023-12-10 22:24:46 +0000

    rubocop soft fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
     name = params[:name]
 
     if name.blank?
-      puts "Name is blank. Authentication failed."
+      puts 'Name is blank. Authentication failed.'
       render json: { error: 'Name is required' }, status: :unprocessable_entity
       return
     end
 
-    user = User.find_by(name: name)
+    user = User.find_by(name:)
 
     if user
       # Authentication successful
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
       render json: { message: 'Authentication successful' }, status: :ok
     else
       # Create a new user
-      new_user = User.create(name: name)
+      new_user = User.create(name:)
 
       if new_user.persisted?
         puts "User created and authenticated successfully for user: #{new_user.name}"
