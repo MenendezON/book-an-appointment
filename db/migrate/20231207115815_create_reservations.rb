132452020a5467c28add05ff4474c9ffa4668494
Mike Dark <agyemankojo47@gmail.com>
2023-12-10 22:34:45 +0000

    Hard fix rubocop

--- db/migrate/20231207115815_create_reservations.rb
+++ db/migrate/20231207115815_create_reservations.rb
@@ -1,7 +1,5 @@
 class CreateReservations < ActiveRecord::Migration[7.1]
   def change
-    create_table :reservations do |t|
-      t.timestamps
-    end
+    create_table :reservations, &:timestamps
   end
 end
