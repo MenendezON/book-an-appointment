1be1bb2c20f5134477e10247aea9bb758c6f7118
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-12 00:37:41 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,28 @@
+# config/routes.rb
 Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
-
+ 
   namespace :api do
     namespace :v1 do
       resources :motorbikes
     end
   end
-
+ 
   # Redirect to the login page if not authenticated
   get '/login', to: 'main#index', constraints: ->(req) { req.session[:user_id].nil? }
   post '/login', to: 'users#authenticate' # Add a separate route for the login action
-
+ 
   # Defines the root path route ("/")
   root 'main#index'
-
+ 
   resources :motorbikes, only: %i[show]
-
+ 
   # Add new route for "My Reservations"
   get '/my_reservations', to: 'reservations#my_reservations'
-end
+ 
+  #  new route for creating a reservation
+  post '/reservations', to: 'reservations#create'
+ end
+ 
