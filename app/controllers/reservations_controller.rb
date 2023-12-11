b726efa018b2f84550d2d96b8a188e36775a3d09
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-12 00:41:10 +0200

    add actions in reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,3 +1,23 @@
-def my_reservations
-  @reservations = current_user.reservations
+# app/controllers/reservations_controller.rb
+class ReservationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def my_reservations
+    @reservations = current_user.reservations
+  end
+
+  def create
+    @reservation = current_user.reservations.create(reservation_params)
+    if @reservation.save
+      redirect_to my_reservations_path, notice: 'Reservation was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:date, :city)
+  end
 end
