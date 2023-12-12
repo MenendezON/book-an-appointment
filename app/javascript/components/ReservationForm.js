643934bbb542e869455169cb3850e6f4c6e5d4fa
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-12 11:55:30 +0200

    Add inputs in reg. form

--- app/javascript/components/ReservationForm.js
+++ app/javascript/components/ReservationForm.js
@@ -5,24 +5,29 @@ import "react-datepicker/dist/react-datepicker.css";
 const ReservationForm = () => {
  const [date, setDate] = useState(new Date());
  const [city, setCity] = useState('');
+ const [motorbike, setMotorbike] = useState(''); // Add this line
 
  const handleSubmit = (event) => {
-  event.preventDefault();
-  // Make a POST request to the Rails API to create the reservation
+ event.preventDefault();
+ // Make a POST request to the Rails API to create the reservation
  };
 
  return (
-  <form onSubmit={handleSubmit}>
-    <label>
-      Date:
-      <DatePicker selected={date} onChange={(date) => setDate(date)} />
-    </label>
-    <label>
-      City:
-      <input type="text" value={city} onChange={(event) => setCity(event.target.value)} />
-    </label>
-    <input type="submit" value="Reserve" />
-  </form>
+ <form onSubmit={handleSubmit}>
+   <label>
+     Date:
+     <DatePicker selected={date} onChange={(date) => setDate(date)} />
+   </label>
+   <label>
+     City:
+     <input type="text" value={city} onChange={(event) => setCity(event.target.value)} />
+   </label>
+   <label>
+     Motorbike:
+     <input type="text" value={motorbike} onChange={(event) => setMotorbike(event.target.value)} /> // Add this line
+   </label>
+   <input type="submit" value="Reserve" />
+ </form>
  );
 };
 
