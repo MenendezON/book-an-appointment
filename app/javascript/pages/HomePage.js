c8e467703e08397cfd80ada17517369ba5814fa3
Mike Dark <agyemankojo47@gmail.com>
2023-12-12 21:41:54 +0000

    Refactor Homepage

--- app/javascript/pages/HomePage.js
+++ app/javascript/pages/HomePage.js
@@ -16,7 +16,7 @@ const Home = () => {
   const handleShowLess = () => {
     setDisplayedItems(3);
   };
-  
+
   if (isLoading) {
     return (
       <h1>Loading</h1>
@@ -33,46 +33,58 @@ const Home = () => {
   if (content) {
     return (
       <>
-      <Navigation />
-        <section>
-        <div className='left_nav'>
-        {displayedItems > 3 ? (
-          <div class="btn active" onClick={handleShowLess}>&lt;</div>
-        ) : (
-          <div class="btn inactive" >&lt;</div>
-        )}
-        </div>
-          <div className="cards">
-            <div className='top_header'>
-              <h2>Latest models</h2>
-              <p>Please select a Motorbike Model</p>
-              <div className='dashed-line'>&nbsp;</div>
+        <div className="container">
+          <Navigation />
+          <section>
+            <div className="left_nav">
+              {displayedItems > 3 ? (
+                <div className="btn active" onClick={handleShowLess}>&lt;</div>
+              ) : (
+                <div className="btn inactive">&lt;</div>
+              )}
             </div>
-            <div className='bottom_content'>
-              {content.slice(displayedItems-3, displayedItems).map((stat) => (
-              <Link to={`./motorbikes/${stat.id}`} key={stat.id} className="card">
-                <div className="avatar">
-                  <img src={stat.image} alt="" />
-                </div>
-                <div>
-                  <p>{stat.name} {stat.model}</p>
-                </div>
-                <div className='dashed-line'>&nbsp;</div>
-                <div className="desc">
-                  <p>{stat.name} {stat.model} is our heritage model and boots the classical look with all the modern goods.</p>
-                </div>
-              </Link>
-            ))}
+            <div className="cards">
+              <div className="top_header">
+                <h2>Latest models</h2>
+                <p>Please select a Motorbike Model</p>
+                <div className="dashed-line">&nbsp;</div>
+              </div>
+              <div className="bottom_content">
+                {content.slice(displayedItems - 3, displayedItems).map((stat) => (
+                  <Link to={`./motorbikes/${stat.id}`} key={stat.id} className="card">
+                    <div className="avatar">
+                      <img src={stat.image} alt="" />
+                    </div>
+                    <div>
+                      <p>
+                        {stat.name}
+                        {' '}
+                        {stat.model}
+                      </p>
+                    </div>
+                    <div className="dashed-line">&nbsp;</div>
+                    <div className="desc">
+                      <p>
+                        {stat.name}
+                        {' '}
+                        {stat.model}
+                        {' '}
+                        is our heritage model and boots the classical look with all the modern goods.
+                      </p>
+                    </div>
+                  </Link>
+                ))}
+              </div>
             </div>
+            <div className="right_nav">
+              {content.length > 3 && displayedItems < content.length ? (
+                <div className="btn active" onClick={handleShowMore}>&gt;</div>
+              ) : (
+                <div className="btn inactive">&gt;</div>
+              )}
+            </div>
+          </section>
         </div>
-        <div className='right_nav'>
-        {content.length > 3 && displayedItems < content.length ?(
-          <div class="btn active" onClick={handleShowMore}>&gt;</div>
-          ) : (
-            <div class="btn inactive" >&gt;</div>
-          )}
-          </div>
-        </section>
       </>
     );
   }
