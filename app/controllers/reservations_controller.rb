9c57e8af8daff69c52210daef56fff7a78ee6b39
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-12 11:58:55 +0200

    update reservations controller, fix linter errors

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,13 +1,13 @@
 # app/controllers/reservations_controller.rb
 class ReservationsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def my_reservations
-    @reservations = current_user.reservations
+    @reservations = User.find(session[:user_id]).reservations
   end
 
   def create
-    @reservation = current_user.reservations.create(reservation_params)
+    @reservation = User.find(session[:user_id]).reservations.create(reservation_params)
     if @reservation.save
       redirect_to my_reservations_path, notice: 'Reservation was successfully created.'
     else
