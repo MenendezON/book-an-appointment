81ebdb1ccc03820db0992b7475303a9af788a27f
Mike Dark <agyemankojo47@gmail.com>
2023-12-13 10:48:53 +0000

    Adjust scroll functionality

--- app/javascript/pages/HomePage.js
+++ app/javascript/pages/HomePage.js
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { useSelector } from 'react-redux';
-import {
-  Link,
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Navigation from '../components/Navigation';
 
 const Home = () => {
   const { content, isLoading, error } = useSelector((store) => store.motorbikes);
-  const [displayedItems, setDisplayedItems] = useState(3);
+  const cardsContainerRef = useRef();
 
-  const handleShowMore = () => {
-    setDisplayedItems(content.length);
-  };
+  const handleScroll = (direction) => {
+    const container = cardsContainerRef.current;
+    const containerWidth = container.offsetWidth;
+    const scrollAmount = containerWidth / 3; // Adjust the scroll amount as needed
 
-  const handleShowLess = () => {
-    setDisplayedItems(3);
+    if (direction === 'right') {
+      container.scrollLeft += scrollAmount;
+    } else {
+      container.scrollLeft -= scrollAmount;
+    }
   };
 
   if (isLoading) {
-    return (
-      <h1>Loading</h1>
-    );
+    return <h1>Loading</h1>;
   }
   if (error) {
     return (
       <h1>
         Something went wrong!
-        { error }
+        {error}
       </h1>
     );
   }
@@ -36,52 +36,38 @@ const Home = () => {
         <div className="container">
           <Navigation />
           <section>
-            <div className="left_nav">
-              {displayedItems > 3 ? (
-                <div className="btn active" onClick={handleShowLess}>&lt;</div>
-              ) : (
-                <div className="btn inactive">&lt;</div>
-              )}
+            <div className="left_nav" onClick={() => handleScroll('left')}>
+              <div className="btn active">&lt;</div>
             </div>
-            <div className="cards">
+            <div className="cards-container" ref={cardsContainerRef}>
               <div className="top_header">
                 <h2>Latest models</h2>
                 <p>Please select a Motorbike Model</p>
                 <div className="dashed-line">&nbsp;</div>
               </div>
               <div className="bottom_content">
-                {content.slice(displayedItems - 3, displayedItems).map((stat) => (
+                {content.map((stat) => (
                   <Link to={`./motorbikes/${stat.id}`} key={stat.id} className="card">
                     <div className="avatar">
                       <img src={stat.image} alt="" />
                     </div>
                     <div>
                       <p>
-                        {stat.name}
-                        {' '}
-                        {stat.model}
+                        {stat.name} {stat.model}
                       </p>
                     </div>
-                    <div className="dashed-line">&nbsp;</div>
                     <div className="desc">
                       <p>
-                        {stat.name}
-                        {' '}
-                        {stat.model}
-                        {' '}
-                        is our heritage model and boots the classical look with all the modern goods.
+                        {stat.name} {stat.model} is our heritage model and boots the classical
+                        look with all the modern goods.
                       </p>
                     </div>
                   </Link>
                 ))}
               </div>
             </div>
-            <div className="right_nav">
-              {content.length > 3 && displayedItems < content.length ? (
-                <div className="btn active" onClick={handleShowMore}>&gt;</div>
-              ) : (
-                <div className="btn inactive">&gt;</div>
-              )}
+            <div className="right_nav" onClick={() => handleScroll('right')}>
+              <div className="btn active">&gt;</div>
             </div>
           </section>
         </div>
