a631e43380ea01c3120eb148b8624c4106bd912d
MenendezON <menendezon@gmail.com>
2023-12-14 18:10:50 +0000

    Fix offenses

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,6 +2,6 @@
 config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://127.0.0.1:3000' # Replace with the actual URL of your React app
-    resource '/api/v1/motorbikes', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '/api/v1/motorbikes', headers: :any, methods: %i[get post put patch delete options head]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :motorbikes, only: [:index, :new, :create, :show] do
+      resources :motorbikes, only: %i[index new create show] do
         resources :reservations
       end
       resources :reservations
