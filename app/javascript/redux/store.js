307858f3b26b98da5e4dff567428b3d098213373
Michael Darkwah <agyemankojo47@gmail.com>
2023-12-15 12:46:42 +0000

    Update store.js

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -1,16 +1,18 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import motorbikesReducer from './motorbikes/motorbikeSlice';
-import userAuthReducer from './userAuthSlice';
-
-// Combine reducers
-const rootReducer = combineReducers({
-  motorbikes: motorbikesReducer,
-  userAuth: userAuthReducer,
-});
-
-// Create store with combined reducers
-const store = configureStore({
-  reducer: rootReducer,
-});
-
-export default store;
+ import { configureStore, combineReducers } from '@reduxjs/toolkit';
+ import motorbikesReducer from './motorbikes/motorbikeSlice';
+ import reservationsReducer from './reservation/reservationSlice';
+ import userAuthReducer from './userAuthSlice';
+ 
+ // Combine reducers
+ const rootReducer = combineReducers({
+   motorbikes: motorbikesReducer,
+   reservations: reservationsReducer,
+   userAuth: userAuthReducer,
+ });
+ 
+ // Create store with combined reducers
+ const store = configureStore({
+   reducer: rootReducer,
+ });
+ 
+ export default store;
