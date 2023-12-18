15d165369099fef836777d728832da3ff29716c5
Mike Dark <agyemankojo47@gmail.com>
2023-12-18 03:18:17 +0000

    Update app.js

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,4 +1,4 @@
- import * as React from 'react'; 
+import * as React from 'react'; 
  import { Routes, Route, Navigate } from 'react-router-dom';
  import { useDispatch, useSelector } from 'react-redux';
  import { useEffect } from 'react';
@@ -10,21 +10,12 @@
  import Reservation from '../pages/ReservationPage';
  import AddReservation from '../pages/AddReservation';
  import AddMotorbike from '../pages/AddMotorbike';
- import SignUp from '../pages/Signup'; // Assuming you have a Login component
- import Login from '../pages/LoginPage'; // Assuming you have a Login component
  import '../assets/css/style.css';
-
-import ProtectedRoute from './ProtectionRoute';
+import RemoveMotorbikePage from '../pages/RemoveMotorbike';
  
  const App = () => {
    const dispatch = useDispatch();
-   const loginResponse = useSelector((state) => state.user.user.token);
-   {loginResponse ? (
-    console.log("I'm connected")
-   ):(
-    console.log("I'm on my way to connec")
-   )}
-  // const isAuthenticated = useSelector((state) => state.auth.isAuthenticated); // Update this based on your auth state
+   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
  
    useEffect(() => {
      dispatch(getMotorbikes());
@@ -33,38 +24,14 @@ import ProtectedRoute from './ProtectionRoute';
  
    return (
      <Routes>
-       {loginResponse ? (
-         <>
            <Route path="/" element={<Home />} />
            <Route path="/motorbikes/new" element={<AddMotorbike />} />
            <Route path="/motorbikes/:id" element={<Details />} />
            <Route path="/reservations" element={<Reservation />} />
            <Route path="/reservations/new" element={<AddReservation />} />
-           <Route path="/reservations/list" element={<AddReservation />} />
-         </>
-       ) : (
-         <Route path="/" element={<Navigate to="/login" />} />
-       )}
-       <Route path="/login" element={<Login />} />
-       <Route path="/signup" element={<SignUp />} />
+           <Route path="/reservations/remove" element={<RemoveMotorbikePage />} />
      </Routes>
-
-    // <Routes>
-    //     {/* public Routes */}
-    //     <Route path="/" element={<Login />} />
-    //     <Route path="/signup" element={<SignUp />} />
-    //     {/* private Routes */}
-    //     <Route
-    //       path="/home"
-    //       element={(
-    //         <ProtectedRoute>
-    //           <Home />
-    //         </ProtectedRoute>
-    //     )}
-    //     />
-    //   </Routes>
    );
  };
  
  export default App;
- 
